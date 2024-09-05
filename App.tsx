ad69c850d33a192b69418ff967613b7c5a204dbd
tommaldonado2 <tommaldonado2@gmail.com>
2024-09-05 15:11:29 -0300

    :construction: refactor: Pasta app com dois arquivos novos..

--- App.tsx
+++ App.tsx
@@ -1,6 +1,22 @@
+import { 
+  useFonts,
+  Ubuntu_700Bold,
+  Ubuntu_500Medium,
+  Ubuntu_400Regular 
+} from '@expo-google-fonts/ubuntu'
+
 import { Home } from '@/app/home';
+import { Loading } from '@/app/loading';
 
 export default function App() {
+  const[fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Ubuntu_500Medium,
+    Ubuntu_400Regular
+  })
+
+  if(!fontsLoaded){
+    return <Loading />
+  }
   return <Home />
-  
 }
---
+++ src/app/loading/index.tsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator } from "react-native"
+
+import { styles } from './styles'
+
+export function Loading(){
+    return(
+        <ActivityIndicator style={styles.loading}></ActivityIndicator>
+    )
+}
